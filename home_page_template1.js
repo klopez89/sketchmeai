da741cbe67facc521574b6530920fe7e7a91ed76
klopez <klopez@wayfair.com>
2023-08-09 11:29:40 -0400

    Setup first commit for sketch me ai web app, tweak2

--- home_page_template1.js
+++ home_page_template1.js
@@ -1,69 +1,88 @@
-document.addEventListener('TailwindLoaded', function() {
-  configureHomePage();
-});
 
-function configureHomePage() {
-	addHomePageToDOM();
-	styleButtons();
-}
-
-function styleButtons() {
-	const getStartedButton = document.getElementById('get-started-button');
-  styleEnabledButton([getStartedButton]);
-}
-
-function takeUserToAuth() {
-	window.location.href = base_site_url + auth_page; // Constants from webflow
-}
-
-function addHomePageToDOM() {
-	let home_page_html = homePageHtml();
-	let home_page_div = $($.parseHTML(home_page_html));
-  $('body').append(home_page_div);
-}
 
 function homePageHtml() {
 	return `
-	<div class="relative isolate overflow-hidden custom-dark-gray-bg">
-		<svg class="absolute inset-0 -z-10 h-full w-full stroke-gray-600  [mask-image:radial-gradient(90%_90%_at_top_left,gray,transparent)]" aria-hidden="true">
-			<defs>
-				<pattern id="0787a7c5-978c-4f66-83c7-11c213f99cb7" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
-					<path d="M.5 200V.5H200" fill="none" />
-				</pattern>
-			</defs>
-			<rect width="100%" height="100%" stroke-width="0" fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)" />
-		</svg>
-		
-		<div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
-		
-		
-		
-		
-			<div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg lg:flex-shrink-0 lg:pt-8">
-				<img class="h-11" src="https://storage.googleapis.com/minecraft_maps/bnw_mappy_logo.png" alt="Your Company">
-				<div class="mt-24 sm:mt-32 lg:mt-16">
+	<div class="bg-white">
+	  <header class="absolute inset-x-0 top-0 z-50">
+	    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+	      <div class="flex lg:flex-1">
+	        <a href="#" class="-m-1.5 p-1.5">
+	          <span class="sr-only">Your Company</span>
+	          <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+	        </a>
+	      </div>
+	      <div class="flex lg:hidden">
+	        <button type="button" id="menuButton" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+	          <span class="sr-only">Open main menu</span>
+	          <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+	            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+	          </svg>
+	        </button>
+	      </div>
+	      <div class="hidden lg:flex lg:gap-x-12">
+	        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Product</a>
+	        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Features</a>
+	        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Marketplace</a>
+	        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Company</a>
+	      </div>
+	      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+	        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Log in <span aria-hidden="true">&rarr;</span></a>
+	      </div>
+	    </nav>
+	    <!-- Mobile menu, show/hide based on menu open state. -->
+	    <div id="mobileMenu" class="lg:hidden" role="dialog" aria-modal="true">
+	      <!-- Background backdrop, show/hide based on slide-over state. -->
+	      <div class="fixed inset-0 z-50"></div>
+	      <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+	        <div class="flex items-center justify-between">
+	          <a href="#" class="-m-1.5 p-1.5">
+	            <span class="sr-only">Your Company</span>
+	            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+	          </a>
+	          <button type="button" id="closeMenuButton" class="-m-2.5 rounded-md p-2.5 text-blue-700">
+	            <span class="sr-only">Close menu</span>
+	            <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+	              <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+	            </svg>
+	          </button>
+	        </div>
+	        <div class="mt-6 flow-root">
+	          <div class="-my-6 divide-y divide-gray-500/10">
+	            <div class="space-y-2 py-6">
+	              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Product</a>
+	              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Features</a>
+	              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Marketplace</a>
+	              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Company</a>
+	            </div>
+	            <div class="py-6">
+	              <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	  </header>
 
-				</div>
-				<h1 class="mt-10 text-4xl font-bold text-shadow-style sm:text-6xl">Realm maps in one place.</h1>
-				<p class="mt-6 text-lg leading-8 text-gray-100">Explore your Minecraft Realms in one place with Minecraft Mappy. Easily generate and share maps to plan your next adventure and discover new areas.</p>
-				<div class="mt-10 flex items-center gap-x-6">
+	  <div class="relative isolate pt-4">
 
-					<button id="get-started-button" onClick="takeUserToAuth()" class="text-xl bg-orange-500">
-						<div class="button-title">&nbsp;Get Started&nbsp;</div>
-					</button>
-
-				</div>
-			</div>
-			
-			<div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
-				<div class="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-					<div class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-						<img src="https://storage.googleapis.com/minecraft_maps/dantes_screenshot1-min.png" alt="App screenshot" width="1084" height="1086" class="w-[40rem] rounded-md shadow-2xl ring-1 ring-gray-900/10">
-					</div>
-				</div>
-			</div>
-			
-		</div>
+	    <div class="py-24 sm:py-32 lg:pb-20">
+	      <div class="lg:flex justify-between mx-auto max-w-7xl px-6 lg:px-8 items-center">
+	        <div class="mx-auto max-w-2xl text-center min-w-[40%]">
+	          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your AI sketches start here.</h1>
+	          <p class="mt-6 text-lg leading-8 text-gray-600">SketchMe AI uses the latest Stable Diffusion technology to sketch new images of you based on your uploaded images.</p>
+	          <div class="mt-10 flex items-center justify-center gap-x-6">
+	            <a href="#" class="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Get started</a>
+	            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
+	          </div>
+	        </div>
+	        <div class="mt-4 flow-root sm:mt-4">
+	          <div class="m-5 lg:m-20">
+	            <img src="https://storage.googleapis.com/sketchme_site/homepage_main_image%402x.png" alt="App screenshot" width="2432" height="1442" class="">
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
 	</div>
 	`;
 }

646a241c12e69a038bf34357be8f6ffd00b76c15
klopez <klopez@wayfair.com>
2023-08-10 11:57:37 -0400

    lost something, tweak3

--- home_page_template6.js
+++ home_page_template6.js
@@ -76,7 +76,7 @@ function homePageHtml() {
 	          </div>
 	        </div>
 	        <div class="mt-4 flow-root sm:mt-4">
-	          <div class="my-10 mx-16 md:mx-16 lg:mx-10">
+	          <div class="my-10 mx-0 md:mx-16 lg:mx-10">
 	            <img src="https://storage.googleapis.com/sketchme_site/homepage_main_image%402x.png" alt="App screenshot" width="2432" height="1442" class="max-h-min">
 	          </div>
 	        </div>

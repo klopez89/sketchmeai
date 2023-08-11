1f48a90904c7d291ce12088cc50ce9f6b5ae5f65
Kevin Lopez <klopez@bu.edu>
2023-08-11 02:49:51 -0400

    lost something, tweak4

---
+++ shared_templates1.js
@@ -0,0 +1,18 @@
+
+function loader() {
+	let loaderHTML = `
+	<div id="loader" class="loader-overlay">
+		<i class="fa fa-spinner fa-spin loading-spinner"></i>
+	</div>
+	`;
+	return loaderHTML
+}
+
+function centeredLoader_HTML() {
+	let loaderHTML = `
+	<div id="loader" class="loader-overlay-centered">
+		<i class="fa fa-spinner fa-spin loading-spinner-centered"></i>
+	</div>
+	`;
+	return loaderHTML
+}

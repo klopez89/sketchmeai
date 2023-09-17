adddfaaf036c045cb8f8e90f593c896425c25b69
Kevin Lopez <klopez@bu.edu>
2023-09-17 18:42:06 -0400

    Added support files for the image set page, tweak4

--- image_set_template2.js
+++ image_set_template2.js
@@ -3,6 +3,31 @@
 function mainPromptRow() {
   let row =
   `
+  <div
+    data-te-lightbox-init
+    class="flex flex-col space-y-5 lg:flex-row lg:space-x-5 lg:space-y-0">
+    <div class="h-full w-full">
+      <img
+        src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Slides/1.webp"
+        data-te-img="https://mdbcdn.b-cdn.net/img/Photos/Slides/1.webp"
+        alt="Table Full of Spices"
+        class="w-full cursor-zoom-in data-[te-lightbox-disabled]:cursor-auto" />
+    </div>
+    <div class="h-full w-full">
+      <img
+        src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Slides/2.webp"
+        data-te-img="https://mdbcdn.b-cdn.net/img/Photos/Slides/2.webp"
+        alt="Winter Landscape"
+        class="w-full cursor-zoom-in data-[te-lightbox-disabled]:cursor-auto" />
+    </div>
+    <div class="h-full w-full">
+      <img
+        src="https://mdbcdn.b-cdn.net/img/Photos/Thumbnails/Slides/3.webp"
+        data-te-img="https://mdbcdn.b-cdn.net/img/Photos/Slides/3.webp"
+        alt="View of the City in the Mountains"
+        class="w-full cursor-zoom-in data-[te-lightbox-disabled]:cursor-auto" />
+    </div>
+  </div>
   <div class="main-prompting-row" id="mainPromptingRow">
   </div>
   `;

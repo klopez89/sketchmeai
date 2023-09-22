66780e4afd26212d4f7f27b576c805f88f366dbb
klopez <klopez@wayfair.com>
2023-09-22 16:00:04 -0400

    finished up the imageset page and now switching over to purchase page again

--- js/helpers/shared_templates.js
+++ js/helpers/shared_templates.js
@@ -8,10 +8,13 @@ function loader() {
 	return loaderHTML
 }
 
-function centeredLoader_HTML() {
+function centeredLoader_HTML(loadingText = '') {
 	let loaderHTML = `
-	<div id="loader" class="loader-overlay-centered">
-		<i class="fa fa-spinner fa-spin loading-spinner-centered"></i>
+	<div id="loader" class="absolute top-0 left-0 flex items-center justify-center h-full w-full bg-white">
+		<div class="flex flex-col items-center gap-4">
+			<p class="text-2xl">${loadingText}</p>
+			<i class="text-4xl fa fa-spinner fa-spin"></i>
+		</div>
 	</div>
 	`;
 	return loaderHTML

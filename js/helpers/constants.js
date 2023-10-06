948550b71a40b72e33fc94935517da64c39f5f99
klopez <klopez@wayfair.com>
2023-10-05 20:24:37 -0400

    added to the constants file

--- js/helpers/constants.js
+++ js/helpers/constants.js
@@ -1,3 +1,14 @@
 function PlaceholderImageStr() {
 	return 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
 }
+
+function getCurrentPageUrlWithoutQueryParams() {
+    var url = window.location.href;
+    return url.split('?')[0];
+}
+
+function hasDevSubdomain() {
+    var url = window.location.href;
+    var hostname = new URL(url).hostname;
+    return hostname.startsWith('dev.');
+}

b5248abe1bdc07b778cc8b193365674ec291a997
Kevin Lopez <klopez@bu.edu>
2023-11-19 02:53:12 -0500

    combined some logic when authstatechanges in purchase page, tweak15

--- js/pages/purchase/purchase_page.js
+++ js/pages/purchase/purchase_page.js
@@ -3,6 +3,10 @@ initializePage();
 
 // Initialize the FirebaseUI Widget using Firebase.
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
+if (ui.isPendingRedirect()) {
+	ui.start('#firebaseui-auth-container', uiConfig);
+  }
+
 var logoutPressed = false;
 var wasJustPendingRequest = false;
 

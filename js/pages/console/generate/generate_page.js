30033658f5ce5f00af653a129547d649c5d3f666
klopez <klopez@wayfair.com>
2024-01-04 19:45:26 -0500

    updating aspect ratio code, tweak3

---
+++ js/pages/console/generate/generate_page.js
@@ -0,0 +1,11 @@
+
+
+function addImageGrid() {
+    let dummy_grid_html = dummyGridHTML();
+    let dummy_grid_div = $($.parseHTML(dummy_grid_html));
+    $('#console-content').append(dummy_grid_div);
+}
+
+window.onload = function() {
+    addImageGrid();
+}

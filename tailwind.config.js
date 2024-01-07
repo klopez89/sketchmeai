d7a66e12059ee7cdf19eb0cef6db7596e2ae4621
Kevin Lopez <klopez@bu.edu>
2024-01-07 14:10:08 -0500

    added generate form to left of image grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './**/*.{html,js}',
+    "./js/**/*.js",
+    "./*.html",
+    "./imageset/*.html",
   ],
   theme: {},
   corePlugins: {
     aspectRatio: false,
-    width: true,
-    height: true,
-    textColor: true,
-    size: true,
-    fontSize: true,
-    backgroundColor: true,
-    backgroundOpacity: true,
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
   ],
 }
-
-// "./js/**/*.js",
-// "./*.html",
-// "./imageset/*.html",

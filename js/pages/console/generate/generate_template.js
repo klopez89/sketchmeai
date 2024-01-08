9fa646db40d6de42e65a2192f7951219e24eac36
klopez <klopez@wayfair.com>
2024-01-08 03:02:16 -0500

    working on getting a new generation working again, tweak8

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -56,7 +56,7 @@ function dummyGridHTML() {
                     </div>
                 </div>
                 <div class="col-span-full flex justify-center" id="gen-button-container">
-                    <button class="rounded-md flex-grow text-center bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" onClick="generateButtonPressed()">Generate</button>
+                    <input type="submit" class="rounded-md flex-grow text-center bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Generate</input>
                 </div>
 
                 <div class="col-span-full" id="neg-prompt-field-container">

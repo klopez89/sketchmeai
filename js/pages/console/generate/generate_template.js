43e16deb68d356747a29465e25e71bf6429286e8
klopez <klopez@wayfair.com>
2024-01-13 01:29:33 -0500

    supporting generation, tweak60

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -14,7 +14,7 @@ function newGridItemHTML(promptInfo) {
     return `
     <li class="relative rounded-lg overflow-hidden" generation-id="${promptInfo.generationId}">
         <div class="group aspect-h-10 aspect-w-10 block w-full">
-            <img src="" alt="" class="pointer-events-none object-cover hidden">
+            <img src="" alt="" class="pointer-events-none object-cover group-hover:opacity-75 hidden">
             <div id="gen-loader" class="bg-gray-200 flex justify-center items-center">
                 <i class="fa fa-spinner fa-spin text-4xl text-gray-500" aria-hidden="true"></i>
                 <p class="absolute bottom-0 right-0 pb-2 pr-2 text-xs text-gray-500" id="gen-status">...queued</p>

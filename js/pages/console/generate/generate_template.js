36250f36bf5fde63f5a00aa17e8112e08c469792
klopez <klopez@wayfair.com>
2024-01-13 01:13:09 -0500

    supporting generation, tweak59

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -13,7 +13,7 @@ const imageUrls = [
 function newGridItemHTML(promptInfo) {
     return `
     <li class="relative rounded-lg overflow-hidden" generation-id="${promptInfo.generationId}">
-        <div class="aspect-h-10 aspect-w-10 block w-full">
+        <div class="group aspect-h-10 aspect-w-10 block w-full">
             <img src="" alt="" class="pointer-events-none object-cover hidden">
             <div id="gen-loader" class="bg-gray-200 flex justify-center items-center">
                 <i class="fa fa-spinner fa-spin text-4xl text-gray-500" aria-hidden="true"></i>

fb4064a12ea8201d8e4a5ee0e9cc48a92855d55b
klopez <klopez@wayfair.com>
2024-01-14 15:57:45 -0500

    supporting generation, tweak72

--- js/pages/console/generate/generate_page.js
+++ js/pages/console/generate/generate_page.js
@@ -293,9 +293,9 @@ function addImageGrid() {
     let dummy_grid_div = $($.parseHTML(dummy_grid_html));
     $('#console-content').append(dummy_grid_div);
 
-    let dummy_grid_data_html = createGridHTML(20);
-    let dummy_grid_data_div = $($.parseHTML(dummy_grid_data_html));
-    $('#collection-grid').append(dummy_grid_data_div);
+    // let dummy_grid_data_html = createGridHTML(20);
+    // let dummy_grid_data_div = $($.parseHTML(dummy_grid_data_html));
+    // $('#collection-grid').append(dummy_grid_data_div);
 }
 
 function resizeGrid() {

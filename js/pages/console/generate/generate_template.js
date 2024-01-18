86a624d1346a80f29982dde04ec364d52cff12a8
klopez <klopez@wayfair.com>
2024-01-17 23:01:06 -0500

    supporting generation, tweak178

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -1,10 +1,5 @@
 
-sample_gen = null;
-
 function newGenItem_FromExistingGen(generation) {
-    if(sample_gen == null) {
-        sample_gen = generation;
-    }
     gen_string = JSON.stringify(generation);
     return newGridItemHTML(generation.rec_id, gen_string);
 }
@@ -20,7 +15,7 @@ function newGridItemHTML(gen_id, gen_string="") {
             <div id="gen-loader" class="bg-gray-200 flex justify-center items-center">
                 <i class="fa fa-spinner fa-spin text-4xl text-gray-500" aria-hidden="true"></i>
                 <p class="absolute bottom-0 right-0 pb-2 pr-2 text-xs text-gray-500" id="gen-status"></p>
-                <button class="absolute top-0 right-0 p-2 text-xs text-gray-500 hidden" id="cancel-button">Cancel</button>
+                <button class="absolute top-0 right-0 p-2 text-xs text-blue-500 hidden" id="cancel-button">Cancel</button>
             </div>
             <button id="main-gen-button" type="button" class="absolute inset-0 focus:outline-none pointer-events-none"></button>
             <div id="copy-container" class="bg-transparent pointer-events-none group hidden">

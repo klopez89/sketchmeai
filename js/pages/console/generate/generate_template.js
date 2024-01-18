ea8c8d7f237415dc9a510bfee30d28f1a33639c9
klopez <klopez@wayfair.com>
2024-01-18 02:57:11 -0500

    supporting generation, tweak181

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -163,7 +163,7 @@ function dummyGridHTML() {
         <div class="flex-1 xl:flex">
             <div class="bg-gray-100 px-0 py-0 xl:flex-1">
                 <div class="bg-gray-100">
-                    <div id="collection-grid-container" class="relative mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8 overflow-y-auto">
+                    <div data-te-lightbox-init id="collection-grid-container" class="relative mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8 overflow-y-auto">
                         <ul id="collection-grid" role="list" class="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"></ul>
                         <div id="grid-loader" class="absolute top-0 left-0 w-full h-full px-4 py-8 sm:px-6 lg:px-8">
                             <div class="bg-gray-100 w-full h-full flex justify-center items-start">

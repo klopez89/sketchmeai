efc34ff693915fc9310a06b5d73de5986c30d23c
klopez <klopez@wayfair.com>
2024-02-03 00:25:27 -0500

    working on the models page to kick off new training and show that its in progress, tweak4

--- js/pages/console/models/models_template.js
+++ js/pages/console/models/models_template.js
@@ -1,13 +1,13 @@
 function dummyGridHTML() {
     return `
     <div class="bg-gray-100">
-        <div id="collection-grid-container" class="relative mx-auto max-w-7xl px-2 py-6 overflow-y-auto" style="height: 1073px;">
+        <div id="collection-grid-container" class="relative mx-auto max-w-7xl px-2 py-6 overflow-y-auto h-full">
             <div id="collection-grid" role="list" class="grid grid-cols-2 gap-x-2 gap-y-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" @click.away="clickedOutsideOfGenMenu()" onclick="clickedOnEmptyPartOfGrid()">
                 <div class="relative rounded-lg overflow-hidden" generation-id="p5csGOlasuX7syWZNPaP" gen-info="{" blob_path":"i5uwekabqs0uojvecdck="" p5csgolasux7sywznpap_pz2s7wdb37zwg36evyvligr4ti.png","collection_id":null,"gen_recipe":{"guidance_scale":13,"high_noise_frac":0.9,"img2img_url":"","inference_steps":20,"lora_scale":0.6,"neg_prompt":"","prompt":"cartoon="" of="" goofy="" blue="" velociraptor,="" closeup="" near="" the="" right="" half="" face,="" retro,="" shiny="" scales,="" rainforest="" in="" background","prompt_strength":0.8,"refine":"no_refiner","res_height":1024,"res_width":1024,"scheduler":"k_euler","seed":"34941870"},"generation_time":5.540187,"model_name":"stability-ai="" sdxl","model_rec_id":"","model_version":"39ed52f2a78e934b3ba6e2a89f5b1c712de7dfea535525255b1aa35c5565e08b","prediction_error":null,"prediction_status":"succeeded","rec_id":"p5csgolasux7sywznpap","replicate_prediction_id":"pz2s7wdb37zwg36evyvligr4ti","signed_gen_url":"https:="" storage.googleapis.com="" dev-sketchmeai-prediction-images="" i5uwekabqs0uojvecdck="" 30="" jan="" 2024="" 18:22:06="" gmt","user_rec_id":"i5uwekabqs0uojvecdck"}"="" style="">
 
-                    <div class="aspect-[1/1] bg-black">
-                        <div class="flex justify-center items-center h-full">
-                            <i class="fas fa-plus"></i>
+                    <div class="aspect-[1/1] bg-gray-300 hover:bg-gray-200 cursor-pointer">
+                        <div class="flex justify-center items-center h-full text-white text-5xl">
+                            <i class="fas fa-plus" aria-hidden="true"></i>
                         </div>
                     </div>
                 </div>

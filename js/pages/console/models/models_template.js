eb07612645807127b99372ed30441cf4699c6d66
klopez <klopez@wayfair.com>
2024-02-04 23:36:09 -0500

    working on the models page to kick off new training and show that its in progress, tweak44

--- js/pages/console/models/models_template.js
+++ js/pages/console/models/models_template.js
@@ -195,7 +195,7 @@ function dummyGridHTML() {
                     </div>
                     <div class="col-span-full flex flex-col justify-center" id="gen-button-container">
 
-                        <button id="uploadToServerButton" type="submit" value="Generate" class="rounded-md flex-grow-0 flex-shrink-0 text-center border border-transparent px-3.5 py-2.5 text-base font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-600 hover:bg-blue-500">
+                        <button id="uploadToServerButton" type="submit" value="Generate" class="rounded-md flex-grow-0 flex-shrink-0 text-center border border-transparent px-3.5 py-2.5 text-base font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-200 hover:bg-gray-200" disabled>
                             <p>Begin Fine Tuning Model</p>
                             <i class="fa fa-spinner fa-spin" style="position: absolute; display:none;" aria-hidden="true"></i>
                         </button>

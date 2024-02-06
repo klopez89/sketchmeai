76ec3b483cdd7c525188513792a6a883cbb3b6ac
klopez <klopez@wayfair.com>
2024-02-06 00:37:36 -0500

    working on the models page to kick off new training and show that its in progress, tweak76

--- js/pages/console/models/models_page.js
+++ js/pages/console/models/models_page.js
@@ -536,7 +536,7 @@ function exitNewModelForm(event) {
     collectionGridContainer.classList.add('opacity-100');
     setTimeout(() => {
         collectionGridContainer.classList.remove('hidden');
-    }, 150);
+    }, 600);
 
     let newFormContainer = document.getElementById('new-form-container');
     newFormContainer.classList.add('transform', 'translate-x-full', 'transition-all', 'duration-500', 'ease-in-out');

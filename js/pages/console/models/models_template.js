f65ed752c872daa1af27bc3900413f7a0c32c1fe
Kevin Lopez <klopez@bu.edu>
2024-02-06 02:29:24 -0500

    working on the models page to kick off new training and show that its in progress, tweak103

--- js/pages/console/models/models_template.js
+++ js/pages/console/models/models_template.js
@@ -74,7 +74,7 @@ function dummyGridHTML() {
                     <div class="col-span-2" id="token-string-field-container">
                         <label for="token-string" class="block text-sm font-medium leading-6 text-gray-900">Token String</label>
                         <div class="mt-2">
-                            <input type="text" name="token-string" id="token-string" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            <input type="text" name="token-string" id="token-string" value="zxc" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div class="col-span-2" id="seed-field-container">

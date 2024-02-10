8bcfe551a6c6f31d6c341e82bdcf09ed758b7dfd
Kevin Lopez <klopez@bu.edu>
2024-02-09 21:48:03 -0500

    working on the models page to kick off new training and show that its in progress, tweak128

--- js/pages/console/models/models_template.js
+++ js/pages/console/models/models_template.js
@@ -248,7 +248,7 @@ function uploadEntryDiv(file, is_first_file) {
 
             <img src="" alt="" class="object-cover group-hover:opacity-95 transition-opacity duration-200 cursor-pointer opacity-100" onclick="showModelMenu(event)">
 
-            <div class="aspect-[1/1] bg-gray-300 hover:bg-gray-900">
+            <div id="model-name-container" class="aspect-[1/1] bg-gray-300 hover:bg-gray-900">
                 <div class="flex justify-center items-center h-full text-white text-5xl">
                     <p id="model-name" class="text-black text-2xl"></p>
                 </div>

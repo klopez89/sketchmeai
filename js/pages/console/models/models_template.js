19c8d624fbc3369fd7cba0ade677105c3c19de9a
klopez <klopez@wayfair.com>
2024-02-12 12:58:19 -0500

    working on the models page to kick off new training and show that its in progress, tweak153

--- js/pages/console/models/models_template.js
+++ js/pages/console/models/models_template.js
@@ -172,7 +172,7 @@ function dummyGridHTML() {
                         <label for="mixed-precision" class="block text-sm font-medium leading-6 text-gray-900">Mixed Precision</label><div class="mt-2">
                             <select id="mixed-precision" name="mixed-precision" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                 <option>fp16</option>
-                                <option>fp32</option>
+                                <option>bf16</option>
                                 <option>None</option>
                             </select>
                         </div>

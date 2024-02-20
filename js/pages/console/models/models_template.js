de41ed6d9858b472c8ad5c08602aac2261ca6015
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-20 03:02:45 -0500

    working on hiding advanced params for generation and then the new model form, tweak49

--- js/pages/console/models/models_template.js
+++ js/pages/console/models/models_template.js
@@ -34,6 +34,7 @@ function dummyGridHTML() {
                     <div class="col-span-2" id="name-field-container">
                         <div class="flex justify-between items-center">
                             <label for="model-name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                            <p class="text-xs text-gray-500 italic pl-1">(no spaces)</p>
                             <div class="text-xs text-red-500 italic hidden" id="model-name-validation">
                                 Model name is already taken.
                             </div>

7d91a04bbb87ae31defbff39d4f026786ac33d3e
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-19 22:37:52 -0500

    working on hiding advanced params for generation and then the new model form, tweak35

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -70,7 +70,7 @@ function dummyGridHTML() {
                     <div class="col-span-full" id="prompt-field-container">
                         <label for="prompt" class="block text-sm font-medium leading-6 text-gray-900">Prompt</label>
                         <div class="mt-2">
-                            <textarea id="prompt" name="prompt" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" style="margin-top: 0px; margin-bottom: 0px; height: 110px;"></textarea>
+                            <textarea id="prompt" name="prompt" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" style="margin-top: 0px; margin-bottom: 0px; height: 110px;"></textarea>
                         </div>
                     </div>
 	

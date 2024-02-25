f2cb4aa673aacdf1ed0d10038c4d6405a9d39364
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-25 16:31:19 -0500

    revamping how model name is highlighted in prompt field and then swapped for instance key, tweak39

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -70,7 +70,7 @@ function dummyGridHTML() {
                     <div class="col-span-full" id="prompt-field-container">
                         <label for="prompt" class="block text-sm font-medium leading-6 text-gray-900">Prompt</label>
                         <div class="mt-2">
-							<textarea id="prompt" name="prompt" rows="3" class="max-w-full bg-white whitespace-normal editable break-words outline-none px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" style="margin-top: 0px; margin-bottom: 0px; height: 110px;"></textarea>
+							<div id="prompt" name="prompt" rows="3" class="max-w-full bg-white whitespace-normal editable break-words outline-none px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" style="margin-top: 0px; margin-bottom: 0px; height: 110px;"></div>
                         </div>
                     </div>
 	

c917f08508199fcbc94f15364b395950d19c32e3
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-25 23:16:04 -0500

    reworking auth to go from home to the new console experience, tweak5

--- js/pages/auth/auth_templates.js
+++ js/pages/auth/auth_templates.js
@@ -1,9 +1,13 @@
 function firebaseUI_HTML() {
 	let firebase_ui_html = `
 	<div class="px-8 flex flex-col flex-grow h-full">
-		<div class="flex flex-col flex-grow">
-			<div class="relative block w-full p-12 flex flex-col flex-grow justify-center">
+		<div class="flex flex-col flex-grow relative">
 
+            <div id="loader" class="absolute opacity-100 bg-transparent w-full h-full flex justify-center items-center transition duration-500 z-10">
+                <i class="fa fa-spinner fa-spin text-4xl text-gray-500" aria-hidden="true"></i>
+            </div>
+
+			<div id="auth-area" class="hidden opacity-0 relative block w-full p-12 flex flex-col flex-grow justify-center transition duration-500">
                 <div class="bg-white sm:px-0 mb-12">
                     <div class="flex flex-col items-center justify-between sm:flex-nowrap">
                         <div class="text-center">

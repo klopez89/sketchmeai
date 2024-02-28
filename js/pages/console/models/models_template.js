926d31d5441826e749c210e42902670b8f680b2e
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-28 02:02:52 -0500

    making it possible for users to upload HEIC files since that file type is common from iphones, tweak10

--- js/pages/console/models/models_template.js
+++ js/pages/console/models/models_template.js
@@ -3,11 +3,11 @@ function dummyGridHTML() {
     <div class="bg-gray-100 h-full relative flex justify-center">
         <div id="collection-grid-container" class="absolute mx-auto max-w-4xl px-8 py-8 overflow-y-auto h-full w-full bg-gray-100 transform transition-all duration-200 ease-in-out">
             <div class="relative">
-                <div id="collection-grid" role="list" class="grid grid-cols-2 gap-x-2 gap-y-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" @click.away="clickedOutsideOfCollectionGrid()" onclick="clickedOnEmptyPartOfGrid()">
+                <div id="collection-grid" role="list" class="grid grid-cols-2 gap-x-2 gap-y-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3" @click.away="clickedOutsideOfCollectionGrid()" onclick="clickedOnEmptyPartOfGrid()">
                     <div id="new-model-button" class="relative rounded-lg overflow-hidden cursor-pointer" onclick="clickedOnNewModelButton(event)">
                         <div class="aspect-[1/1] bg-black hover:bg-gray-800">
-                            <div class="flex justify-center items-center h-full text-white text-5xl">
-                                <i class="fas fa-plus" aria-hidden="true"></i>
+                            <div class="flex justify-center items-center h-full text-white text-2xl">
+                                Train<br>New Model
                             </div>
                         </div>
                     </div>
@@ -15,7 +15,7 @@ function dummyGridHTML() {
 
                 <div id="grid-loader" class="absolute top-0 left-0 w-full h-full">
                     <div class="bg-gray-100 w-full h-full flex justify-center items-start">
-                        <div class="mt-5">    
+                        <div class="mt-5">
                             <i class="fa fa-spinner fa-spin text-4xl text-gray-500 mt-5" aria-hidden="true"></i>
                         </div>
                     </div>

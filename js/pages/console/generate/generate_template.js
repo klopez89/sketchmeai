d6fe4c348baf66b7b9519be0249e15e1ec563d62
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-29 17:12:34 -0500

    putting the img2img fields in its own accordion, tweak7

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -204,7 +204,7 @@ function dummyGridHTML() {
 									<!-- Start of the new nested accordion for img-2img-url and prompt-strength fields -->
 									<div id="nestedAccordion">
 										<h2 id="nestedHeading">
-											<button class="group relative flex items-center rounded-t-[15px] border-0 bg-transparent pt-2 text-right text-sm text-gray-700 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none [&amp;:not([data-te-collapse-collapsed])]:bg-transparent [&amp;:not([data-te-collapse-collapsed])]:text-gray-400" type="button" data-te-collapse-init="" data-te-collapse-toggle="" data-te-target="#nestedCollapse" aria-expanded="false" aria-controls="nestedCollapse" data-te-collapse-collapsed="">Img to Img
+											<button class="group relative flex items-center rounded-t-[15px] border-0 bg-transparent pt-2 text-right text-sm text-gray-700 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none [&amp;:not([data-te-collapse-collapsed])]:bg-transparent [&amp;:not([data-te-collapse-collapsed])]:text-gray-400" type="button" data-te-collapse-init="" data-te-collapse-toggle="" data-te-target="#nestedImg2ImgCollapse" aria-expanded="false" aria-controls="nestedCollapse" data-te-collapse-collapsed="">Show Image To Image fields
 
 												<span class="ml-2 mt-0 h-4 w-4 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
 													<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">

7ba4731748d117b5723a17b98b339a4cedac8f3d
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-29 16:17:31 -0500

    improving generation form validation before firing off gen api, tweak14

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -131,7 +131,7 @@ function dummyGridHTML() {
 										<i class="fa-solid fa-circle-info" aria-hidden="true"></i>
 									</button>
 									<div class="mt-2">
-										<input type="number" name="denoising-steps" id="denoising-steps" min="1" max="500" value="20" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+										<input type="number" name="denoising-steps" id="denoising-steps" placeholder="20" min="1" max="500" value="20" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
 										<p class="text-right text-xs text-gray-400 mt-1 ml-1">1 - 100</p>
 									</div>
 								</div>
@@ -152,7 +152,7 @@ function dummyGridHTML() {
 										<i class="fa-solid fa-circle-info" aria-hidden="true"></i>
 									</button>
 									<div class="mt-2">
-									<input type="number" name="guidance-scale" id="guidance-scale" min="1.0" max="20.0" step="0.1" value="13" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+									<input type="number" name="guidance-scale" id="guidance-scale" placeholder="13" min="1.0" max="20.0" step="0.1" value="13" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
 									<p class="text-right text-xs text-gray-400 mt-1 ml-1">1.0 - 20.0</p>
 									</div>
 								</div>

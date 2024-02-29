f90dde1f505f484d82d6e742365dd12921234f71
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-29 16:22:05 -0500

    improving generation form validation before firing off gen api, tweak15

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -175,7 +175,7 @@ function dummyGridHTML() {
 										<i class="fa-solid fa-circle-info" aria-hidden="true"></i>
 									</button>
 									<div class="mt-2">
-										<input type="text" name="img-2-img-url" id="img-2-img" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+										<input type="text" name="img-2-img-url" id="img-2-img" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" autocomplete="off">
 									</div>
 								</div>
 								<div class="sm:col-span-3" id="ps-field-container">

81eae00fa782300022105fb88617aff423d56c56
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-02-29 16:49:09 -0500

    putting the img2img fields in its own accordion, tweak4

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -203,7 +203,7 @@ function dummyGridHTML() {
 								<div class="col-span-full">
 									<!-- Start of the new nested accordion for img-2img-url and prompt-strength fields -->
 									<div id="nestedAccordion">
-										<h2 id="nestedHeading">
+										<h2 id="nestedHeading" class="flex flex-row items-center">
 											<span class="mr-2 h-5 w-5 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
 												<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
 												<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"></path>

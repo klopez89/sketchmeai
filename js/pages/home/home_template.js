5cfa02e6576e239902875b91c353b70ae5546132
Kevin Lopez <Kev@miguels-mbp.myfiosgateway.com>
2024-03-05 02:09:34 -0500

    added the faq section to homepage, tweak9

--- js/pages/home/home_template.js
+++ js/pages/home/home_template.js
@@ -225,7 +225,7 @@ function homePageHtml() {
 			</div>
 		</div>
 	  
-		<div class="bg-white py-24 sm:py-32">
+		<div class="bg-white py-24">
 			<div class="mx-auto max-w-7xl px-6 lg:px-8">
 				<div class="mx-auto max-w-2xl lg:text-center mb-12">
 					<p class="mt-2 text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl">Roadmap</p>

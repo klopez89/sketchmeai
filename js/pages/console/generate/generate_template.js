675f7ee2be457139c81cf6e8c66e06364c161398
klopez <klopez@wayfair.com>
2024-03-18 18:45:51 -0400

    updating how placeholder prompt shifts to regular prompt when hitting gen without an intended prompt, tweak22

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -144,7 +144,8 @@ function dummyGridHTML() {
 										<i class="fa-solid fa-circle-info" aria-hidden="true"></i>
 									</button>
 									<div class="mt-2">
-									<textarea id="neg-prompt" name="neg-prompt" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" style="margin-top: 0px; margin-bottom: 0px; height: 80px;"></textarea>
+									<textarea id="neg-prompt" name="neg-prompt" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" style="margin-top: 0px; margin-bottom: 0px; height: 80px;">
+									ugly, morbid, photorealistic</textarea>
 									</div>
 								</div>
 

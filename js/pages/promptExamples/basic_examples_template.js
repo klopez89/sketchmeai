849843c14cb4edfee7a223a270770bc5864007b8
klopez <klopez@wayfair.com>
2024-03-19 16:23:57 -0400

    adding the first basic version of basic examples page, tweak4

--- js/pages/promptExamples/basic_examples_template.js
+++ js/pages/promptExamples/basic_examples_template.js
@@ -3,18 +3,68 @@ function basicExamplesHTML() {
     <div class="bg-gray-100 h-full relative flex justify-center">
         <div id="examples-container" class="mx-auto max-w-4xl px-8 py-8 h-full w-full bg-gray-100 transform transition-all duration-200 ease-in-out">
             <div class="flex flex-col">
-                <div class="flex flex-col md:flex-row p-4">
-                    <img src="https://storage.googleapis.com/sketchmeai-public/base_images/leather/leather1.png" alt="Image description" class="w-full md:w-1/2">
-                    <div class="text md:w-1/2">
-                    Your text goes here.
+
+                <div class="flex flex-col md:flex-row items-start p-4 gap-4">
+                    <div class="w-full md:w-1/2">
+                        <div class="aspect-w-10 aspect-h-10 overflow-hidden">
+                            <img src="https://storage.googleapis.com/sketchmeai-public/prompt_examples/home_img8.png" alt="Image description" class="object-cover w-full h-full">
+                        </div>
+                    </div>
+                    <div id="prompt-values" class="md:w-1/2">
+                        <div id="value-set">
+                            <header3 class="text-lg font-semibold">Prompt</header3>
+                            <div class="text-medium text-black">closeup portrait graphic image of Kevin with soft smile and sleek red-colored leather jacket, style is bold, sharp outlines and solid borders around each shape, reminiscent of cell-shading in classic animation and graphic novels</div>
+                            </div>
+                                            <div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Negative Prompt</header3>
+                            <div class="text-medium text-black">morbid, photorealistic, ugly, teeth</div>
+                            </div><div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Denoising Steps</header3>
+                            <div class="text-medium text-black">20</div>
+                            </div><div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Guidance Scale</header3>
+                            <div class="text-medium text-black">13</div>
+                            </div><div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Lora Scale</header3>
+                            <div class="text-medium text-black">0.9</div>
+                            </div><div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Seed</header3>
+                            <div class="text-medium text-black">382414040</div>
+                        </div>
                     </div>
                 </div>
-                <div class="flex flex-col md:flex-row p-4">
-                    <img src="https://storage.googleapis.com/sketchmeai-public/base_images/leather/leather1.png" alt="Image description" class="w-full md:w-1/2">
-                    <div class="text md:w-1/2">
-                    Your text goes here.
+
+                <div class="flex flex-col md:flex-row items-start p-4 gap-4">
+                    <div class="w-full md:w-1/2">
+                        <div class="aspect-w-10 aspect-h-10 overflow-hidden">
+                            <img src="https://storage.googleapis.com/sketchmeai-public/prompt_examples/home_img8.png" alt="Image description" class="object-cover w-full h-full">
+                        </div>
+                    </div>
+                    <div id="prompt-values" class="md:w-1/2">
+                        <div id="value-set">
+                            <header3 class="text-lg font-semibold">Prompt</header3>
+                            <div class="text-medium text-black">closeup portrait graphic image of Kevin with soft smile and sleek red-colored leather jacket, style is bold, sharp outlines and solid borders around each shape, reminiscent of cell-shading in classic animation and graphic novels</div>
+                            </div>
+                                            <div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Negative Prompt</header3>
+                            <div class="text-medium text-black">morbid, photorealistic, ugly, teeth</div>
+                            </div><div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Denoising Steps</header3>
+                            <div class="text-medium text-black">20</div>
+                            </div><div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Guidance Scale</header3>
+                            <div class="text-medium text-black">13</div>
+                            </div><div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Lora Scale</header3>
+                            <div class="text-medium text-black">0.9</div>
+                            </div><div id="value-set" class="mt-2">
+                                        <header3 class="text-lg font-semibold">Seed</header3>
+                            <div class="text-medium text-black">382414040</div>
+                        </div>
                     </div>
                 </div>
+
+
             </div>
         </div>
     </div>

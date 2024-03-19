2a0e56c59f0c5de68ea20fd4184739b6a2b63927
klopez <klopez@wayfair.com>
2024-03-19 16:48:07 -0400

    adding the first basic version of basic examples page, tweak5

--- js/pages/promptExamples/basic_examples_page.js
+++ js/pages/promptExamples/basic_examples_page.js
@@ -1,9 +1,20 @@
 
 addBasicExamplesHTMLToDOM();
-
+generateExamples();
 
 function addBasicExamplesHTMLToDOM() {
     let basic_examples_html = basicExamplesHTML();
     let basic_examples_div = $($.parseHTML(basic_examples_html));
     $('body').append(basic_examples_div);
+}
+
+function generateExamples() {
+    const exampleDictionaries = example_dictionaries;
+    const examplesContainer = $('#examples-container');
+
+    exampleDictionaries.forEach(example => {
+        const newExample = newPromptExample(example);
+        const newExampleElement = $($.parseHTML(newExample));
+        examplesContainer.append(newExampleElement);
+    });
 }

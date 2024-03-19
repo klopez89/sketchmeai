48d74528f91d655a24da4ba171dbcdbd46a142a8
Kevin Lopez <klopez@bu.edu>
2024-03-19 03:08:30 -0400

    adding the first basic version of basic examples page, tweak3

--- js/pages/promptExamples/basic_examples_template.js
+++ js/pages/promptExamples/basic_examples_template.js
@@ -4,13 +4,13 @@ function basicExamplesHTML() {
         <div id="examples-container" class="mx-auto max-w-4xl px-8 py-8 h-full w-full bg-gray-100 transform transition-all duration-200 ease-in-out">
             <div class="flex flex-col">
                 <div class="flex flex-col md:flex-row p-4">
-                    <img src="https://storage.googleapis.com/sketchmeai-prediction-images/MIMu5PUnoVOIhydMmR3T5GN89k82/ZMGNfKqKeH0PHeLUNkDT_6plr7gdbtwjtlribly3lsjwhuu.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&amp;X-Goog-Credential=sketchmeai-service-account%40sketchmeai-396920.iam.gserviceaccount.com%2F20240319%2Fauto%2Fstorage%2Fgoog4_request&amp;X-Goog-Date=20240319T064713Z&amp;X-Goog-Expires=7199&amp;X-Goog-SignedHeaders=host&amp;response-content-disposition=attachment%3B%20filename%3Dai-image-output-1.png&amp;X-Goog-Signature=7f512a2cd90b8e9ef148fb794b53bcd3c5a6295aab7c1907453e6708dc4601f102d9d924c201a889e5474cc82af7a3a46ec8105767079dc8f7d93528eb1d0ee86917bc3f639675b151d87e1a1233adc81f39aaec005c6071ff2df163d0206de2fcd303ef625413b850fe009069b0f9d1af22cdffb1c85ef1d2eead22707eae7c1bcd023a8bb8c7f2566932d1d4371ca042623656cebc66da996f0b9946986ff89bd46a1a92c71f1ed2c80584a23f662dc3d8e150f8e39136eda0ff500ae55eca6cc3a6995aafcb96c653fdff3d68fcf378a5467301bb247e8925b41c76106b603d3c778c1c8efd28e335643fbd04833ca6e4957d3233e0a0f56153b09b64adaf" alt="Image description" class="w-full md:w-1/2">
+                    <img src="https://storage.googleapis.com/sketchmeai-public/base_images/leather/leather1.png" alt="Image description" class="w-full md:w-1/2">
                     <div class="text md:w-1/2">
                     Your text goes here.
                     </div>
                 </div>
                 <div class="flex flex-col md:flex-row p-4">
-                    <img src="https://storage.googleapis.com/sketchmeai-prediction-images/MIMu5PUnoVOIhydMmR3T5GN89k82/ZMGNfKqKeH0PHeLUNkDT_6plr7gdbtwjtlribly3lsjwhuu.png?X-Goog-Algorithm=GOOG4-RSA-SHA256&amp;X-Goog-Credential=sketchmeai-service-account%40sketchmeai-396920.iam.gserviceaccount.com%2F20240319%2Fauto%2Fstorage%2Fgoog4_request&amp;X-Goog-Date=20240319T064713Z&amp;X-Goog-Expires=7199&amp;X-Goog-SignedHeaders=host&amp;response-content-disposition=attachment%3B%20filename%3Dai-image-output-1.png&amp;X-Goog-Signature=7f512a2cd90b8e9ef148fb794b53bcd3c5a6295aab7c1907453e6708dc4601f102d9d924c201a889e5474cc82af7a3a46ec8105767079dc8f7d93528eb1d0ee86917bc3f639675b151d87e1a1233adc81f39aaec005c6071ff2df163d0206de2fcd303ef625413b850fe009069b0f9d1af22cdffb1c85ef1d2eead22707eae7c1bcd023a8bb8c7f2566932d1d4371ca042623656cebc66da996f0b9946986ff89bd46a1a92c71f1ed2c80584a23f662dc3d8e150f8e39136eda0ff500ae55eca6cc3a6995aafcb96c653fdff3d68fcf378a5467301bb247e8925b41c76106b603d3c778c1c8efd28e335643fbd04833ca6e4957d3233e0a0f56153b09b64adaf" alt="Image description" class="w-full md:w-1/2">
+                    <img src="https://storage.googleapis.com/sketchmeai-public/base_images/leather/leather1.png" alt="Image description" class="w-full md:w-1/2">
                     <div class="text md:w-1/2">
                     Your text goes here.
                     </div>

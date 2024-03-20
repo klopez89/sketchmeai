f15a3976d9882f3c614f2c13791980c35037b3da
Kevin Lopez <klopez@bu.edu>
2024-03-20 05:58:52 -0400

    updated auth page title, tweak2

--- js/pages/console/console.js
+++ js/pages/console/console.js
@@ -217,7 +217,7 @@ function stylePayButtonWith(value) {
     console.log('int value: ', parseInt(value));
     const hasDecimals = parseFloat(value) % 1 != 0;
 
-    if (parseInt(value) < 5 || isNaN(parseInt(value)) || hasDecimals) {
+    if (parseInt(value) < 0 || isNaN(parseInt(value)) || hasDecimals) {
         payButton.setAttribute('disabled', true);
         payButton.classList.remove('bg-black');
         payButton.classList.add('bg-gray-500');

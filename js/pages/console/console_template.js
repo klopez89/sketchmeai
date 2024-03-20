8202a5b168aa2138411e28581307b873af7f9506
klopez <klopez@wayfair.com>
2024-03-20 03:18:41 -0400

    showing a loader from auth to console navigations, tweak5

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -96,37 +96,39 @@ function consoleHtml() {
             <div class="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true"></div>
     
             <div class="flex flex-1 gap-x-4 self-stretch lg:gap-x-6 items-center">
-                <div id="page-title" class="relative flex flex-1 self-center text-xl font-bold">Generate</div>
+                <div id="page-title" class="relative flex flex-0 self-center text-xl font-bold">Generate</div>
                 <button id="basic-examples-button" onclick="navigateToBasicPromptExamples()" class="hidden cursor-pointer border-2 border-black rounded-md flex-grow-0 flex-shrink-0 text-center px-8 py-2.5 text-sm text-black shadow-sm hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">See Prompt Examples</button>                
-                <button id="show-payment-button" onclick="showPaymentModal()" class="rounded-md text-xs text-gray-400 hover:bg-gray-100 px-4 py-4 my-2 relative flex items-center justify-center">
-                    <span id="credit-balance-label" class="inset-0 flex items-center justify-center text-transparent">Credit: $0.00</span>
-                    <i id="balance-spinner" class="fas fa-spinner fa-spin absolute m-auto" aria-hidden="true"></i>
-                </button>
-                <div class="flex items-center gap-x-4 lg:gap-x-6">
-                    
-        
-                    <!-- Separator -->
-                    <div class="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true"></div>
-        
-                    <!-- Profile dropdown -->
-                    <div x-data="Components.menu({ open: false })" x-init="init()" @keydown.escape.stop="open = false; focusButton()" @click.away="onClickAway($event)" class="relative">
-                        <button type="button" class="-m-1.5 flex items-center p-1.5" id="user-menu-button" x-ref="button" @click="onButtonClick()" @keyup.space.prevent="onButtonEnter()" @keydown.enter.prevent="onButtonEnter()" aria-expanded="true" aria-haspopup="true" x-bind:aria-expanded="open.toString()" @keydown.arrow-up.prevent="onArrowUp()" @keydown.arrow-down.prevent="onArrowDown()">
-                            <span class="sr-only">Open user menu</span>
-                            <div id="user-avatar-circle" class="flex h-8 w-8 flex-shrink-0 items-center justify-center bg-black rounded-full text-sm font-medium text-white">A</div>
-                            <span class="hidden lg:flex lg:items-center">
-                            <span id="display-name-label" class="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">Tom Cook</span>
-                            <svg class="ml-2 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd"></path>
-                            </svg>
-                            </span>
-                        </button>
+                
+                <div class="ml-auto flex items-center gap-x-4 lg:gap-x-6">
+                    <button id="show-payment-button" onclick="showPaymentModal()" class="rounded-md text-xs text-gray-400 hover:bg-gray-100 px-4 py-4 my-2 relative flex items-center justify-center">
+                        <span id="credit-balance-label" class="inset-0 flex items-center justify-center text-transparent">Credit: $0.00</span>
+                        <i id="balance-spinner" class="fas fa-spinner fa-spin absolute m-auto" aria-hidden="true"></i>
+                    </button>
+                    <div class="flex items-center gap-x-4 lg:gap-x-6">
+                        <!-- Separator -->
+                        <div class="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-900/10" aria-hidden="true"></div>
             
-                        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none" x-ref="menu-items" x-description="Dropdown menu, show/hide based on menu state." x-bind:aria-activedescendant="activeDescendant" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1" @keydown.arrow-up.prevent="onArrowUp()" @keydown.arrow-down.prevent="onArrowDown()" @keydown.tab="open = false" @keydown.enter.prevent="open = false; focusButton()" @keyup.space.prevent="open = false; focusButton()">
-                            <a class="block px-3 py-1 text-sm leading-6 text-gray-900 cursor-pointer" :class="{ 'bg-gray-50': activeIndex === 1 }" role="menuitem" tabindex="-1" id="user-menu-item-1" @mouseenter="onMouseEnter($event)" @mousemove="onMouseMove($event, 1)" @mouseleave="onMouseLeave($event)" @click="open = false; focusButton(); signOutButtonPressed()">Sign out</a>
+                        <!-- Profile dropdown -->
+                        <div x-data="Components.menu({ open: false })" x-init="init()" @keydown.escape.stop="open = false; focusButton()" @click.away="onClickAway($event)" class="relative">
+                            <button type="button" class="-m-1.5 flex items-center p-1.5" id="user-menu-button" x-ref="button" @click="onButtonClick()" @keyup.space.prevent="onButtonEnter()" @keydown.enter.prevent="onButtonEnter()" aria-expanded="true" aria-haspopup="true" x-bind:aria-expanded="open.toString()" @keydown.arrow-up.prevent="onArrowUp()" @keydown.arrow-down.prevent="onArrowDown()">
+                                <span class="sr-only">Open user menu</span>
+                                <div id="user-avatar-circle" class="flex h-8 w-8 flex-shrink-0 items-center justify-center bg-black rounded-full text-sm font-medium text-white">A</div>
+                                <span class="hidden lg:flex lg:items-center">
+                                <span id="display-name-label" class="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">Tom Cook</span>
+                                <svg class="ml-2 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd"></path>
+                                </svg>
+                                </span>
+                            </button>
+                
+                            <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none" x-ref="menu-items" x-description="Dropdown menu, show/hide based on menu state." x-bind:aria-activedescendant="activeDescendant" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1" @keydown.arrow-up.prevent="onArrowUp()" @keydown.arrow-down.prevent="onArrowDown()" @keydown.tab="open = false" @keydown.enter.prevent="open = false; focusButton()" @keyup.space.prevent="open = false; focusButton()">
+                                <a class="block px-3 py-1 text-sm leading-6 text-gray-900 cursor-pointer" :class="{ 'bg-gray-50': activeIndex === 1 }" role="menuitem" tabindex="-1" id="user-menu-item-1" @mouseenter="onMouseEnter($event)" @mousemove="onMouseMove($event, 1)" @mouseleave="onMouseLeave($event)" @click="open = false; focusButton(); signOutButtonPressed()">Sign out</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </div>
     
         <main id="console-container" class="py-0 flex-grow overflow-auto relative">

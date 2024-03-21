1fff67e94e5234211d37e392f7a1610701761a6c
Kevin Lopez <klopez@bu.edu>
2024-03-21 10:17:49 -0400

    optimizing for mobie web, tweak9

--- js/pages/console/generate/generate_page.js
+++ js/pages/console/generate/generate_page.js
@@ -929,10 +929,12 @@ function addBaseGenMenu() {
 }
 
 function resizeGrid() {
-    const leftColumnHeight = document.getElementById('generateForm').clientHeight;
-    const adjustedForPaddingHeight = leftColumnHeight + 2 * 16;
-    // Set the max-height of the collection grid container to the height of the left column
-    document.getElementById('collection-grid-container').style.height = adjustedForPaddingHeight + 'px';
+    if (window.innerWidth >= 768) { // Tailwind's medium size breakpoint is 768px
+        const leftColumnHeight = document.getElementById('generateForm').clientHeight;
+        const adjustedForPaddingHeight = leftColumnHeight + 2 * 16;
+        // Set the max-height of the collection grid container to the height of the left column
+        document.getElementById('collection-grid-container').style.height = adjustedForPaddingHeight + 'px';
+    }
 }
 
 function configureInfiniteScroll() {

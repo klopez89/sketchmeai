54f6f1c7d45d1b672e14cec99438b59220ad89cb
Kevin Lopez <klopez@bu.edu>
2024-03-22 02:16:10 -0400

    made site logo in sidebar link to homepage

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -32,7 +32,7 @@ function consoleHtml() {
         <div id="static-sidebar" class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-48 lg:flex-col">
             <!-- Sidebar component, swap this element with another sidebar if you like -->
             <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-black px-6 pb-4">
-                <div class="flex h-16 shrink-0 items-center">
+                <div class="flex h-16 shrink-0 items-center cursor-pointer" onclick="navigationToHomePage()">
                 <img class="h-8 w-auto" src="https://storage.googleapis.com/sketchmeai-public/branding/white_sketchmeai_logo.png" alt="Your Company">
                 </div>
                 <nav class="flex flex-1 flex-col">

04b71202063ad1f3520407b761dae8ebd433def7
Kevin Lopez <klopez@bu.edu>
2024-03-22 04:22:33 -0400

    updated home header copy, tweak2

--- js/pages/home/home_template.js
+++ js/pages/home/home_template.js
@@ -116,7 +116,7 @@ function homePageHtml() {
 			<div class="pt-24">
 				<div class="lg:flex justify-between mx-auto max-w-7xl px-6 lg:px-8 items-center">
 					<div class="m-5 lg:m-10 mx-auto max-w-2xl text-center min-w-[40%]">
-						<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">${headerTitle}</h1>
+						<h1 class="text-4xl tracking-tight text-gray-900 sm:text-6xl">${headerTitle}</h1>
 						<p class="mt-6 text-lg leading-8 text-secondary text-justify">${headerSubtitle}</p>
 						<div class="mt-10 flex items-center justify-center gap-x-6">
 							<a href="${headerButtonUrl}" class="rounded-md bg-black px-8 py-2.5 text-lg text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">${headerButtonTitle}</a>

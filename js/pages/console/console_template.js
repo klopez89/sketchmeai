afebc6a8fbc4a8ca6bda47050ec40cc32e02e373
klopez <klopez@wayfair.com>
2024-03-25 03:35:18 -0400

    revamping generation settings for mobile web users, tweak18

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -330,7 +330,7 @@ function consoleHtml() {
 
             <!-- Your content -->
     
-                <button class="fixed right-0 bottom-0 m-4 bg-blue-500 text-white rounded-full z-30 w-12 h-12 flex items-center justify-center" @click="$refs.genSettings.open = !$refs.genSettings.open">
+                <button class="fixed right-0 bottom-0 m-4 bg-blue-500 text-white rounded-full z-30 w-12 h-12 flex items-center justify-center" @click="open = !open">
                     <i class="fa-solid fa-wrench"></i>
                 </button>
 

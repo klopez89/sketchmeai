69ebe8d3c9992cff35cd887eace78293209e6f4a
klopez <klopez@wayfair.com>
2024-03-25 14:45:24 -0400

    revamping generation settings for mobile web users, tweak36

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -281,7 +281,7 @@ function successBannerHTML(message) {
 
 function mobileSideBarHTML() {
     return `
-            <div id="mobile-sidebar" x-show="open" x-transition:enter="transition-opacity ease-linear duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition-opacity ease-linear duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed bg-gray-800 top-0 right-0 w-full h-full overflow-auto z-50 flex lg:hidden">
+            <div id="mobile-sidebar" x-show="open" x-transition:enter="transition-opacity ease-linear duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition-opacity ease-linear duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" class="fixed bg-gray-800 bg-opacity-50 top-0 right-0 w-full h-full overflow-auto z-50 flex lg:hidden">
                 
                 <div class="relative max-w-md ml-auto flex-grow flex-1" x-show="open" x-transition:enter=" transition ease-in-out duration-300 transform" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
                     

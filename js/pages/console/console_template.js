3a1204f4336bc66a2298ebef2477fce33e57610a
Kevin Lopez <klopez@bu.edu>
2024-03-25 04:23:00 -0400

    revamping generation settings for mobile web users, tweak26

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -277,7 +277,7 @@ function successBannerHTML(message) {
     `;
 }
     
-function mobileSideBar() {
+function mobileSideBarHTML() {
     return `
             <div id="mobile-sidebar" x-ref="genSettings" x-show="open" x-transition:enter="transition ease-in-out duration-300 transform" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full" class="absolute top-0 right-0 h-full overflow-auto z-50 flex lg:hidden">
                 

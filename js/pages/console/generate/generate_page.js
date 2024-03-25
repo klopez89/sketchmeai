b93aab75d7c82cd10c72f354db3d2e8468cf9399
Kevin Lopez <klopez@bu.edu>
2024-03-25 18:30:56 -0400

    revamping generation settings for mobile web users, tweak65

--- js/pages/console/generate/generate_page.js
+++ js/pages/console/generate/generate_page.js
@@ -827,7 +827,7 @@ function resetGenerateTarget(target) {
         generateIcon.className = 'fa-solid fa-bolt-lightning';
     }
     target.disabled = false;
-    target.blur();
+    target.parentElement.click();
 }
 
 function attemptToShowColdBootingBanner(model_name, is_warmed) {

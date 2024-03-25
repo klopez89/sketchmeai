6c5c0fb92c25fa4183cec3c984e8b8ee724ff8bd
klopez <klopez@wayfair.com>
2024-03-25 03:49:32 -0400

    revamping generation settings for mobile web users, tweak22

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -131,7 +131,7 @@ function consoleHtml() {
 
         </div>
     
-        <main id="console-container" x-data="{ open: false }" class="py-0 flex-grow overflow-auto relative">
+        <main id="console-container" x-data="{ open: false }" :class="{ 'overflow-hidden': open }" class="py-0 flex-grow overflow-auto relative">
 
             <div id="mobile-sidebar" x-ref="genSettings" x-show="open" @open.window="document.body.classList.add('overflow-hidden')" @close.window="document.body.classList.remove('overflow-hidden')" x-transition:enter="transition ease-in-out duration-300 transform" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full" class="absolute inset-0 z-50 flex lg:hidden">
                 

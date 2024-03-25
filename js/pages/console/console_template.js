f87036f71d7c4b5dc9e887d55a22c14fc234b8ba
klopez <klopez@wayfair.com>
2024-03-25 03:20:38 -0400

    revamping generation settings for mobile web users, tweak12

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -133,7 +133,7 @@ function consoleHtml() {
     
         <main id="console-container" class="py-0 flex-grow overflow-auto relative">
 
-            <div id="mobile-sidebar" class="absolute inset-0 z-50 flex lg:hidden">
+            <div id="mobile-sidebar" x-show="open" x-transition:enter="transition ease-in-out duration-300 transform" x-transition:enter-start="-translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="-translate-x-full" class="absolute inset-0 z-50 flex lg:hidden">
                 <!-- Off-canvas menu, show/hide based on off-canvas menu state. -->
                 <div class="relative w-full max-w-full flex-grow flex-1">
                     <!-- Close button -->
@@ -330,7 +330,7 @@ function consoleHtml() {
 
             <!-- Your content -->
     
-                <button class="fixed right-0 bottom-0 m-4 bg-blue-500 text-white rounded-full z-30 w-12 h-12 flex items-center justify-center" @click="open = !open">
+                <button class="fixed right-0 bottom-0 m-4 bg-blue-500 text-white rounded-full z-30 w-12 h-12 flex items-center justify-center" @click="$refs.genSettings.__x.$data.open = true">
                     <i class="fa-solid fa-wrench"></i>
                 </button>
 

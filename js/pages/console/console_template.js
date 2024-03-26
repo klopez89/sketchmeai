ec64a7d679c15c98284c7352bbdd5612c5f27eb4
Kevin Lopez <klopez@bu.edu>
2024-03-26 13:45:36 -0400

    finishing making rest of site mobile friendly, like models and paymnts modal, tweak5

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -291,7 +291,7 @@ function mobileSideBarHTML() {
                 <div class="relative max-w-md ml-auto flex-grow flex-1" x-show="open" x-transition:enter=" transition ease-in-out duration-300 transform" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in-out duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
                     
                     <!-- Sidebar content -->
-                    <div id="sidebar-gen-form-container" class="h-full overflow-y-auto scrolling-touch bg-gray-100 flex flex-col justify-betwee">
+                    <div id="sidebar-gen-form-container" class="h-full overflow-y-auto scrolling-touch bg-gray-100 flex flex-col justify-between">
                         
                         <div class="w-full flex justify-start sticky bottom-0 bg-white p-2 gap-4 border-t border-gray-200" id="gen-button-container">
                             <button id="back-to-models-button" class="pl-3 text-gray-400 hover:text-gray-600 flex-shrink-0" @click="open = !open">

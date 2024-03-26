007c82044a6df4bee84170abc3203e9b1e8d5781
klopez <klopez@wayfair.com>
2024-03-25 21:20:57 -0400

    revamping generation settings for mobile web users, tweak72

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -83,7 +83,7 @@ function consoleHtml() {
             </div>
         </div>
     
-        <div class="lg:pl-48 h-screen flex flex-col">
+        <div class="lg:pl-48 h-full flex flex-col">
         <div class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8">
             <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="open = true">
             <span class="sr-only">Open sidebar</span>
@@ -132,7 +132,7 @@ function consoleHtml() {
         </div>
 
 
-        <div id="console-group" x-data="{ open: false }" :class="{ 'overflow-hidden': open }" class="relative h-screen overflow-auto">
+        <div id="console-group" x-data="{ open: false }" :class="{ 'overflow-hidden': open }" class="relative h-full overflow-auto">
 
 
             <main id="console-container" class="py-0 flex-grow overflow-auto h-full relative">

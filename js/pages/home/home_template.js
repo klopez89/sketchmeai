e588f3a3a77656c92da982b2b7ab12b3b64bfede
klopez <klopez@wayfair.com>
2024-03-27 01:40:51 -0400

    adding webcon

--- js/pages/home/home_template.js
+++ js/pages/home/home_template.js
@@ -366,7 +366,7 @@ function homePageHtml() {
 
 						<div class="col-span-full" id="name-field-container">
 							<div class="flex items-center">
-								<label for="model-name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+								<label for="user-name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
 							</div>
 							<div class="mt-2">
 								<input type="text" name="user-name" id="user-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">

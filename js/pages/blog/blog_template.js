142934bcc0a0507f811bba46b03e03359c8786b3
klopez <klopez@wayfair.com>
2024-03-28 12:52:20 -0400

    adding a blog to the site and first post, tweak5

--- js/pages/blog/blog_template.js
+++ js/pages/blog/blog_template.js
@@ -14,3 +14,28 @@ function blogListHTML() {
     </div>
     `;
 }
+
+function blogListGridHTML() {
+    return `
+    <div id="blog-grid" class="grid grid-cols-2 gap-4">
+        <div class="flex flex-col items-start space-y-4 group cursor-pointer">
+            <div class="aspect-w-1 aspect-h-1 w-full">
+            <img src="https://storage.googleapis.com/sketchmeai-public/Blog/blog_post_1imgs/blog_post1_img39.png" alt="Blog Image 1" class="object-cover">
+            </div>
+            <div>
+            <h2 class="text-xl font-bold group-hover:underline">Getting Started with SketchMeAi: Model Training and Generation Basics</h2>
+            <p class="text-sm text-gray-500">Thur, March 28th 2024</p>
+            </div>
+        </div>
+        <div class="flex flex-col items-start space-y-4">
+            <div class="aspect-w-1 aspect-h-1 w-full">
+            <img src="https://storage.googleapis.com/sketchmeai-public/Blog/blog_post_1imgs/blog_post1_img45.png" alt="Blog Image 2" class="object-cover">
+            </div>
+            <div>
+            <h2 class="text-xl font-bold">Blog Title 2</h2>
+            <p class="text-sm text-gray-500">Published on: <span class="font-medium">Publishing Date 2</span></p>
+            </div>
+        </div>
+    </div>
+    `;
+}

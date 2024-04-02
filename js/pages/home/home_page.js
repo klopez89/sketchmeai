5c8ed47802e5cbea29a594216862710bf70df1c6
Kevin Lopez <klopez@bu.edu>
2024-04-02 00:45:38 -0400

    adding a blog to the site and first post, tweak43

--- js/pages/home/home_page.js
+++ js/pages/home/home_page.js
@@ -13,17 +13,10 @@ function addHomePageToDOM() {
 }
 
 function configureNavMenuButtons() {
-  // Select the menu button
-  const menuButton = document.getElementById('menuButton');
 	// Select the close button inside the mobile slide-over
   const closeButton = document.getElementById('closeMenuButton');
   // Select the mobile slide-over itself
   const mobileMenu = document.getElementById('mobileMenu');
-
-  menuButton.addEventListener('click', function() {
-    // Toggle the visibility of the mobile slide-over when the menu button is clicked
-    mobileMenu.style.display = mobileMenu.style.display === 'none' ? 'block' : 'none';
-  });
 
   closeButton.addEventListener('click', function() {
     // Hide the mobile slide-over when the close button is clicked

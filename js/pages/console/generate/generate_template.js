b5b8aa5b81e3d7b2360c395502e333dcb545edb1
Kevin Lopez <klopez@bu.edu>
2024-04-04 01:44:48 -0400

    prompt str is limited to new min of 0.1

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -276,8 +276,8 @@ function generate_form_html() {
 												<i class="fa-solid fa-circle-info" aria-hidden="true"></i>
 											</button>
 											<div class="mt-2">
-												<input type="number" name="prompt-strength" id="prompt-strength" placeholder="0.8" min="0.0" max="1.0" step="0.1" value="0.8" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
-												<p class="text-right text-xs text-gray-400 mt-1 ml-1">0.0 - 1.0</p>
+												<input type="number" name="prompt-strength" id="prompt-strength" placeholder="0.8" min="0.1" max="1.0" step="0.1" value="0.8" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+												<p class="text-right text-xs text-gray-400 mt-1 ml-1">0.1 - 1.0</p>
 											</div>
 										</div>
 									</div>

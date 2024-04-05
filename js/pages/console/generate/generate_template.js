9e59bd66a147b26d8b56f79f2bee25e466715373
klopez <klopez@wayfair.com>
2024-04-05 01:40:04 -0400

    beginning to add select feature for mass deletion and download, tweak31

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -80,11 +80,11 @@ function dummyGridHTML() {
         <!-- Right column area -->
         <div class="flex-1 md:flex h-full">
             <div class="bg-gray-100 px-0 py-0 md:flex-1 h-full">
-                <div class="bg-gray-100 h-full">
+                <div class="bg-gray-100 h-full relative">
 
-					<div class="mt-3 flex sm:ml-4 sm:mt-0">
+					<div class="absolute bg-gray-500 p-3 z-40 flex w-full justify-end">
 						<button type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="downloadAllButton" onclick="downloadAll()">Download All</button>
-						<button type="button" class="select-to-share-button ml-3 inline-flex items-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" onclick="toggleImageSelectability()">Select to Share</button>
+						<button type="button" class="select-to-share-button ml-3 inline-flex items-center rounded-md px-3 py-2 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 bg-blue-600 text-white hover:bg-blue-500" onclick="toggleImageSelectability()">Select to Share</button>
 					</div>
 
 					<div class="selection-bar fixed bottom-0 w-full right-0 py-5 px-5 bg-white bg-opacity-70 z-50 hidden">

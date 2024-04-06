04baade11bbd79d74a351b47ccd513066fddcc6b
klopez <klopez@wayfair.com>
2024-04-06 10:08:46 -0400

    beginning to add select feature for mass deletion and download, tweak61

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -336,14 +336,20 @@ function new_model_option(model) {
 
 function bottom_generation_menu_html() {
 	return `
-	<div id="mobile-bottom-menu" class="md:hidden fixed right-0 bottom-0 m-4 z-30 flex gap-4">
+	<div id="mobile-bottom-menu" class="md:hidden fixed right-0 bottom-0 m-4 z-30 flex gap-4 items-end">
 		<button class="bg-gray-100 text-black shadow-lg rounded-full w-12 h-12 flex items-center justify-center" @click="open = !open">
 			<i class="fa-solid fa-wrench" aria-hidden="true"></i>
 		</button>
 
-		<button class="bg-black text-white shadow-lg rounded-full w-12 h-12 flex items-center justify-center" onclick="generateButtonPressed(event)">
-			<i id="mobile-gen-icon" class="fa-solid fa-bolt-lightning"></i>
-		</button>
+		<div class="flex flex-col gap-4">
+			<button class="bg-gray-500 text-white shadow-lg rounded-full w-12 h-12 flex items-center justify-center" onclick="newFunction()">
+				<i class="fa-solid fa-ellipsis-vertical" aria-hidden="true"></i>
+			</button>
+
+			<button class="bg-black text-white shadow-lg rounded-full w-12 h-12 flex items-center justify-center" onclick="generateButtonPressed(event)">
+				<i id="mobile-gen-icon" class="fa-solid fa-bolt-lightning" aria-hidden="true"></i>
+			</button>
+		</div>
 	</div>
 	`;
 }

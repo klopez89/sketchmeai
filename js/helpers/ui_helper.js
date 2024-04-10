462bba786495c9eb9096a5e76c667918a1c44a8d
klopez <klopez@wayfair.com>
2024-04-10 13:49:22 -0400

    adding support for collections beyond the default, tweak21

--- js/helpers/ui_helper.js
+++ js/helpers/ui_helper.js
@@ -6,3 +6,14 @@ function animateAway(element, duration) {
         element.classList.add('hidden');
     }, duration);
 }
+
+function animateIn(element) {
+    element.classList.remove('hidden');
+    // Double requestAnimationFrame for browser to have time for a reflow
+    requestAnimationFrame(() => {
+        requestAnimationFrame(() => {
+            element.classList.remove('opacity-0');
+            element.classList.add('opacity-100');
+        });
+    });
+}

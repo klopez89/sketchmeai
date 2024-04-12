74a30b6473eabfb9f90f7805588cf518bdaf2714
Kevin Lopez <klopez@bu.edu>
2024-04-12 08:38:00 -0400

    adding support for collections beyond the default, tweak73

--- js/pages/console/generate/generate_page.js
+++ js/pages/console/generate/generate_page.js
@@ -1442,6 +1442,9 @@ function updateCurrentCollectionLabels() {
     genTopMenuCollectionName.innerHTML = getLastEditedCollectionInfo().collectionName;
 }
 
+function genTopCollectionMenuShowing() {
+    console.log('gen top collection menu showing');
+}
 
 function userWantsToCreateNewCollection() {
     let collectionName = document.getElementById('new-collection-name').value;

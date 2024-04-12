514a84d6e9dbf5ef4d2054ae041eafc4c7bd9b7e
klopez <klopez@wayfair.com>
2024-04-12 13:08:44 -0400

    adding support for collections beyond the default, tweak80

--- js/pages/console/generate/generate_page.js
+++ js/pages/console/generate/generate_page.js
@@ -129,7 +129,8 @@ function configureSelectableDiv(div) {
     if (isSelectable) {
         checkbox.classList.remove("hidden"); 
         selectionOverlay.classList.remove("pointer-events-none");      
-        floatingCopyButton.classList.add("hidden");   
+        floatingCopyButton.classList.add("hidden");
+        floatingCopyButton.classList.remove('md:flex');
     } else {
         div.classList.remove("selected");
         checkbox.classList.add("hidden");
@@ -139,6 +140,7 @@ function configureSelectableDiv(div) {
         const overlay_bg = div.querySelector(".overlay-bg");
         overlay_bg.classList.remove("bg-white", "opacity-50");
         floatingCopyButton.classList.remove("hidden"); 
+        floatingCopyButton.classList.add('md:flex');
     }
 }
 

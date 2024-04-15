6796c6c94b73c34df031b146b2b4eb8b1874e18a
klopez <klopez@wayfair.com>
2024-04-15 13:24:43 -0400

    streamlining the generation settings UI, tweak18

--- js/helpers/constants.js
+++ js/helpers/constants.js
@@ -104,3 +104,15 @@ function isImageUrlExpired(url) {
     // Check if the current date is past the expiration date
     return currentDate > expirationDate;
 }
+
+function summarizeFileSize(sizeInBytes) {
+    if (sizeInBytes >= 1073741824) {
+      return (sizeInBytes / 1073741824).toFixed(2) + " GB";
+    } else if (sizeInBytes >= 1048576) {
+      return (sizeInBytes / 1048576).toFixed(2) + " MB";
+    } else if (sizeInBytes >= 1024) {
+      return (sizeInBytes / 1024).toFixed(2) + " KB";
+    } else {
+      return sizeInBytes + " bytes";
+    }
+  }

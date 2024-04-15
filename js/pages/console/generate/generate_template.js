b49dcdc5f85122f239af8e181d4f081d4ee57972
klopez <klopez@wayfair.com>
2024-04-15 12:46:50 -0400

    streamlining the generation settings UI, tweak15

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -317,7 +317,7 @@ function generate_form_html() {
 							<!-- img-2img-url field -->
 							<div class="col-span-full" id="igm2img-field-container">
 								<div id="ref-img-div-container" class="flex items-center justify-center pt-1 pb-2">
-									<button id="ref-img-button" onclick="triggerLocalUploadForRefImage(event)" class="relative flex flex-col items-center justify-center block w-[8em] h-[8em] rounded-lg border-2 border-dashed border-gray-300 px-12 py-6 text-center hover:border-gray-400 text-gray-300 hover:text-gray-400">
+									<button id="ref-img-button" class="relative flex flex-col items-center justify-center block w-[8em] h-[8em] rounded-lg border-2 border-dashed border-gray-300 px-12 py-6 text-center hover:border-gray-400 text-gray-300 hover:text-gray-400">
 										
 										<img class="absolute w-full h-full rounded-lg" src="">
 

c54bf766b0d50cc277c33dad403b2e1c63f8977b
klopez <klopez@wayfair.com>
2024-04-16 15:47:16 -0400

    streamlining the generation settings UI, tweak42

--- js/pages/console/generate/generate_page.js
+++ js/pages/console/generate/generate_page.js
@@ -797,7 +797,7 @@ function copyPromptInfoFromGen(generation) {
     document.getElementById('guidance-scale').value = generation.gen_recipe.guidance_scale;
     document.getElementById('seed').value = generation.gen_recipe.seed;
     document.getElementById('img-2-img').value = generation.gen_recipe.img2img_url;
-    document.getElementById('prompt-str').value = generation.gen_recipe.prompt_strength;
+    document.getElementById('prompt-str').value = generation.gen_recipe.prompt_strength * 100;
     document.getElementById('lora-scale').value = generation.gen_recipe.lora_scale;
     // Trigger some UI updates in the gen form
     document.getElementById('denoising-steps').dispatchEvent(new Event('input'));

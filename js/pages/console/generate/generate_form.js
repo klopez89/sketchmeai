5e63ee375ac461e163b32dce8caa1281312b41c0
Kevin Lopez <klopez@bu.edu>
2024-04-15 23:21:36 -0400

    streamlining the generation settings UI, tweak30

---
+++ js/pages/console/generate/generate_form.js
@@ -0,0 +1,18 @@
+function configureRefImageFields() {
+    // Get references to the elements
+    var rangeInput = document.getElementById('ref-influence-range');
+    var numberInput = document.getElementById('prompt-str');
+
+    // Set the initial value of the range input to match the number input
+    rangeInput.value = numberInput.value;
+
+    // Update the number input when the range input changes
+    rangeInput.addEventListener('input', function() {
+        numberInput.value = rangeInput.value;
+    });
+
+    // Update the range input when the number input changes
+    numberInput.addEventListener('input', function() {
+        rangeInput.value = numberInput.value;
+    });
+}

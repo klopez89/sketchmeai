c087f5787d049644cd2c8cf801fd2a3bf2e6d636
Kevin Lopez <klopez@bu.edu>
2024-04-18 01:22:09 -0400

    streamlining the generation settings UI, tweak67

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -203,15 +203,15 @@ function generate_form_html() {
 				<div id="lora-component-container" class="col-span-full">
 					<label class="text-sm font-medium leading-6 text-gray-900">Person</label>
 					<div id="lora-person-grid" role="list" class="flex flex-row space-x-2">
-						<div id="new-model-button" class="relative rounded-lg overflow-hidden cursor-pointer" onclick="clickedOnNewModelButton(event)">
-						<div class="aspect-[1/1] bg-black hover:bg-gray-800">
-							<div class="flex justify-center items-center h-full text-white text-2xl">
-								Train<br>New Model
+						<div id="new-model-button" class="relative cursor-pointer" onclick="clickedOnNewModelButton(event)">
+							<div class="w-32 h-32 bg-black hover:bg-gray-800">
+								<div class="flex justify-center items-center h-full text-white text-2xl">
+									None
+								</div>
 							</div>
 						</div>
-						</div>
-						<div class="relative rounded-lg" model-id="6lmgrOLQFwmU3I90aXcH" style="" replicate-name="klopez-sketchmeai/custom_sdxl" version="3b3bfa90aaa92a4b32dc15d000805f9bba176f7759d3d1e7f84e8fa47f686c78">
-						<div class="group w-32 h-32 block relative">
+						<div class="relative cursor-pointer" model-id="6lmgrOLQFwmU3I90aXcH" style="" replicate-name="klopez-sketchmeai/custom_sdxl" version="3b3bfa90aaa92a4b32dc15d000805f9bba176f7759d3d1e7f84e8fa47f686c78">
+						<div class="group w-32 h-32 rounded-lg block relative">
 							<div id="model-name-container" class="aspect-[1/1]" style="background-color: #2e1f37;">
 								<div class="flex justify-left items-end h-full text-white text-5xl">
 									<p id="model-name-label" class="text-xl ml-3 mb-2">Kevin</p>
@@ -224,8 +224,8 @@ function generate_form_html() {
 							</div>
 						</div>
 						</div>
-						<div class="relative rounded-lg overflow-hidden" model-id="xB6tuDiCQ3AVGOF9tkzI" style="" replicate-name="klopez-sketchmeai/custom_sdxl" version="6c0a631505ec1b4e06af74e991d2a7e46276a9b16c018a77d5c6a81031068197">
-						<div class="group w-32 h-32 block relative">
+						<div class="relative cursor-pointer" model-id="xB6tuDiCQ3AVGOF9tkzI" style="" replicate-name="klopez-sketchmeai/custom_sdxl" version="6c0a631505ec1b4e06af74e991d2a7e46276a9b16c018a77d5c6a81031068197">
+						<div class="group w-32 h-32 rounded-lg block relative">
 							<div id="model-name-container" class="aspect-[1/1]" style="background-color: #301f37;">
 								<div class="flex justify-left items-end h-full text-white text-5xl">
 									<p id="model-name-label" class="text-xl ml-3 mb-2">KFive</p>
@@ -238,19 +238,19 @@ function generate_form_html() {
 							</div>
 						</div>
 						</div>
-						<div class="relative rounded-lg overflow-hidden" model-id="ig74e3qCnsO7jdm1S4RR" style="" replicate-name="klopez-sketchmeai/custom_sdxl" version="49665c30531a4f943bd8cbe41e887a1cb040486174de0ce110ef8c154c4e1f07">
-						<div class="group w-32 h-32 block relative">
-							<div id="model-name-container" class="aspect-[1/1]" style="background-color: #1f372b;">
-								<div class="flex justify-left items-end h-full text-white text-5xl">
-									<p id="model-name-label" class="text-xl ml-3 mb-2">Kevo</p>
+						<div class="relative cursor-pointer" model-id="ig74e3qCnsO7jdm1S4RR" style="" replicate-name="klopez-sketchmeai/custom_sdxl" version="49665c30531a4f943bd8cbe41e887a1cb040486174de0ce110ef8c154c4e1f07">
+							<div class="group w-32 h-32 rounded-lg block relative">
+								<div id="model-name-container" class="aspect-[1/1]" style="background-color: #1f372b;">
+									<div class="flex justify-left items-end h-full text-white text-5xl">
+										<p id="model-name-label" class="text-xl ml-3 mb-2">Kevo</p>
+									</div>
+								</div>
+								<div id="model-loader" class="bg-transparent flex justify-center items-center hidden">
+									<i class="fa fa-spinner fa-spin text-4xl text-gray-200" aria-hidden="true"></i>
+									<p class="absolute bottom-0 right-0 pb-3 pr-3 text-xs text-gray-200" id="model-status"></p>
+									<button class="hidden absolute top-0 right-0 p-2 text-xs text-gray-400 hover:text-gray-500" id="cancel-button">Cancel</button>
 								</div>
 							</div>
-							<div id="model-loader" class="bg-transparent flex justify-center items-center hidden">
-								<i class="fa fa-spinner fa-spin text-4xl text-gray-200" aria-hidden="true"></i>
-								<p class="absolute bottom-0 right-0 pb-3 pr-3 text-xs text-gray-200" id="model-status"></p>
-								<button class="hidden absolute top-0 right-0 p-2 text-xs text-gray-400 hover:text-gray-500" id="cancel-button">Cancel</button>
-							</div>
-						</div>
 						</div>
 					</div>
 				</div>

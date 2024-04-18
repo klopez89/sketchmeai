8398b1ff3bc9880a36857d198413f3b5342fa22a
klopez <klopez@wayfair.com>
2024-04-18 10:56:28 -0400

    streamlining the generation settings UI, tweak74

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -196,7 +196,7 @@ function generate_form_html() {
 					<label class="text-sm font-medium leading-6 text-gray-900">Base Model</label>
 				</div>
 				<div class="col-span-4 pr-4" id="base-model-selector-container">
-					<select id="base-model-selector" name="base-model" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+					<select id="base-model-selector" name="base-model" class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
 						<option>SDXL</option>
 					</select>
 				</div>

0068589af895f500a0f931122502c5843b087129
Kevin Lopez <klopez@bu.edu>
2024-04-18 10:57:35 -0400

    streamlining the generation settings UI, tweak75

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -449,7 +449,7 @@ function generate_form_html() {
 							</div>
 
 							<div class="col-span-3" id="mode-selector-container">
-								<select id="gen-count" name="gen-count" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+								<select id="gen-count" name="gen-count" class="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
 									<option>Image to Image</option>
 								</select>
 							</div>

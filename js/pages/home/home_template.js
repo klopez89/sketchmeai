6278b2f6740052d1303a44296118d1092e4b10bf
Kevin Lopez <klopez@bu.edu>
2024-04-23 16:23:13 -0400

    streamlining the generation settings UI, tweak146

--- js/pages/home/home_template.js
+++ js/pages/home/home_template.js
@@ -297,7 +297,7 @@ function homePageHtml() {
 					<p class="mt-2 text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl">Roadmap</p>
 				</div>
 				<div class="mx-auto max-w-7xl px-6 lg:px-8">
-					<div class="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-4">
+					<div class="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-3">
 						
 						<div>
 							<time class="flex items-center text-sm font-semibold leading-6 text-gray-500">

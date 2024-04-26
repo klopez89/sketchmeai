a3da946fda6653c3ff29ca94732093cb2f8912d6
Kevin Lopez <klopez@bu.edu>
2024-04-26 18:44:24 -0400

    adding ays support for imrove prompt comprehension, tweak5

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -641,7 +641,7 @@ function basicGenerationSettingsHTML() {
 						<span class="text-sm text-gray-500" id="availability-description">Enables use of Align Your Steps, a sampling technique that helps improve prompt coherence.</span>
 					</span>
 					<!-- Enabled: "bg-black", Not Enabled: "bg-gray-200" -->
-					<button type="button" onclick="toggleAysPressed(event)" class="bg-gray-200 relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-black-600 focus:ring-offset-2" role="switch" aria-checked="false" aria-labelledby="availability-label" aria-describedby="availability-description">
+					<button id="ays-toggle-button" type="button" onclick="toggleAysPressed(event)" class="bg-gray-200 relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-black-600 focus:ring-offset-2" role="switch" aria-checked="false" aria-labelledby="availability-label" aria-describedby="availability-description">
 						<!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
 						<span aria-hidden="true" class="translate-x-0 pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out"></span>
 					</button>

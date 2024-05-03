2a5022526638c91bc28f63a43f88e4ce1622e214
Kevin Lopez <klopez@bu.edu>
2024-05-03 17:41:24 -0400

    starting blog post 3, tweak2

--- js/pages/blog/post3.js
+++ js/pages/blog/post3.js
@@ -1,11 +1,11 @@
 addComparisons();
 
 function addComparisons() {
-    let blue_before = "";
-    let blue_after = "";
+    let blue_before = "https://storage.googleapis.com/sketchmeai-public/Blog/blog_post_3imgs/blue_jacket_non_ays.png";
+    let blue_after = "https://storage.googleapis.com/sketchmeai-public/Blog/blog_post_3imgs/blue_jacket_ays.png";
 
-    let black_before = "";
-    let black_after = "";
+    let black_before = "https://storage.googleapis.com/sketchmeai-public/Blog/blog_post_3imgs/blue_jacket_non_ays.png";
+    let black_after = "https://storage.googleapis.com/sketchmeai-public/Blog/blog_post_3imgs/blue_jacket_ays.png";
 
     let blue_comp_html = comparisonElementHTML(blue_before, blue_after);
     let blue_comp_div = $($.parseHTML(blue_comp_html));

b4a32ee6d1de19203dda31eb093297e514466a17
klopez <klopez@wayfair.com>
2024-05-03 16:49:26 -0400

    adding comparison slider,tweak3

--- js/helpers/comparison.js
+++ js/helpers/comparison.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.comparison-container');
+const container = document.querySelector('.comp-container');
 document.querySelector('.slider').addEventListener('input', function(e) {
     container.style.setProperty('--position', `${e.target.value}%`);
 });

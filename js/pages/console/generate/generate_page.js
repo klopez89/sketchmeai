83c0f8095a1c2f3d7138d02a2e2391caa7dd15d4
klopez <klopez@wayfair.com>
2024-05-10 21:51:34 -0400

    removed firebase app delete call to see if that removes infinite loop issue, tweak33

--- js/pages/console/generate/generate_page.js
+++ js/pages/console/generate/generate_page.js
@@ -355,9 +355,9 @@ function configureGenerateForm() {
     document.getElementById('neg-prompt').addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
             event.preventDefault();
-            let genButtonElement = document.getElementById('gen-button');
-            let newEvent = new Event('click', { bubbles: false });
-            genButtonElement.dispatchEvent(newEvent);
+            // let genButtonElement = document.getElementById('gen-button');
+            // let newEvent = new Event('click', { bubbles: false });
+            // genButtonElement.dispatchEvent(newEvent);
         }
     });
 

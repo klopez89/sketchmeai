ab5265b39d9ebdb0e7741ef4b8719e6bea37c393
klopez <klopez@wayfair.com>
2024-05-14 01:14:21 -0400

    tweaked gened propts, tweak2

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -640,7 +640,7 @@ function basicGenerationSettingsHTML() {
 								<i class="fa-solid fa-circle-info" aria-hidden="true"></i>
 							</button>
 							<div class="mt-2">
-								<textarea id="neg-prompt" name="neg-prompt" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" style="margin-top: 0px; margin-bottom: 0px; height: 80px;">ugly, morbid, photorealistic</textarea>
+								<textarea id="neg-prompt" name="neg-prompt" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6" style="margin-top: 0px; margin-bottom: 0px; height: 80px;"></textarea>
 							</div>
 						</div>
 

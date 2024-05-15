93f6e6d2605b2f44c20529c71e22fda215caed9b
Kevin Lopez <klopez@bu.edu>
2024-05-15 14:59:39 -0400

    cleaning up for prod launch, tweak9

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -626,7 +626,7 @@ function generateReferenceFormSectionHTML() {
 									<span>Medium</span>
 									<span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
 								</a>
-								<a href="#" class="text-gray-500 rounded-l-lg hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
+								<a href="#" class="text-gray-500 rounded-r-lg hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-center text-sm font-medium hover:bg-gray-50 focus:z-10">
 									<span>High</span>
 									<span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5"></span>
 								</a>

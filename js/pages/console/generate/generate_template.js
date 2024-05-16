0f5c1e8e9885846187df8b620a51c20b5cd27382
klopez <klopez@wayfair.com>
2024-05-16 13:18:20 -0400

    cleaning up for prod launch, tweak27

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -757,7 +757,7 @@ function basicGenerationSettingsHTML() {
 									<i class="fa-solid fa-circle-info" aria-hidden="true"></i>
 								</button>
 								<div class="mt-2">
-									<input type="number" disabled name="denoising-steps" id="denoising-steps" placeholder="10" min="4" max="500" value="10" class="block w-full rounded-md border-0 py-1.5 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
+									<input type="number" disabled name="denoising-steps" id="denoising-steps" placeholder="20" min="4" max="500" value="20" class="block w-full rounded-md border-0 py-1.5 text-gray-400 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6">
 									<p class="text-right text-xs text-gray-400 mt-1 ml-1">4 - 100</p>
 								</div>
 						</div>

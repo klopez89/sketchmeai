a2c6b49ae7c7edb1b66f0ff335e950f51173619c
klopez <klopez@wayfair.com>
2024-05-22 17:38:18 -0400

    making huge phones render well such as samsung s23 ultra, tweak18

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -40,7 +40,7 @@ function newGridItemHTML(gen_id, gen_string="") {
                     <i class="fa fa-copy" aria-hidden="true"></i>
                 </button>
 
-				<button id="favorite-button" class="absolute w-10 h-10 md:w-16 md:h-16 aspect-w-1 aspect-h-1 flex bottom-0 md:bottom-2 left-0 md:left-2 p-2 text-2xl md:text-3xl text-white border-0 border-white opacity-100 rounded-lg pointer-events-auto items-center justify-center transition-opacity duration-200">
+				<button id="favorite-button" class="absolute w-10 h-10 md:w-14 md:h-14 aspect-w-1 aspect-h-1 flex bottom-2 md:bottom-3 left-2 md:left-3 p-2 text-xl md:text-2xl text-white border-0 border-white opacity-100 rounded-lg pointer-events-auto items-center justify-center transition-opacity duration-200">
 
 					<div class="flex flex-grow items-center justify-center text-center">
 						<span class="flex items-center justify-center pr-2 pt-2">

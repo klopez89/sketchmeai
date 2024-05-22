3ce6cd7b0d3865e14172264383c9ada4d670b85c
Kevin Lopez <klopez@bu.edu>
2024-05-22 13:49:57 -0400

    tweaked boundary width in which we decide between mobile and desktop layout in generation page, tweak6

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -324,7 +324,7 @@ function generate_form_html() {
 			</div>
 		</div>
 
-		<div class="col-span-full flex-col px-4 pb-2 pt-3 justify-center hidden md:flex border-t border-gray-300" id="gen-button-container">
+		<div class="col-span-full flex-col px-4 pb-2 pt-3 justify-center hidden lg:flex border-t border-gray-300" id="gen-button-container">
 			<button id="gen-button" class="cursor-pointer rounded-md flex flex-grow justify-center items-center text-center bg-black px-3.5 py-2.5 text-lg text-white shadow-sm hover:bg-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" onclick="generateButtonPressed(event)">
 				<p id="desktop-gen-button-label" class="flex items-center">Generate</p>
 				<i class="fa fa-spinner fa-spin hidden absolute" aria-hidden="true"></i>

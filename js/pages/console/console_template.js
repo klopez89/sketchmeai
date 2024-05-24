06fde9a6827ad0d03c6c7b1e2ef93ce57f124996
klopez <klopez@wayfair.com>
2024-05-24 15:03:34 -0400

    making huge phones render well such as samsung s23 ultra, tweak84

--- js/pages/console/console_template.js
+++ js/pages/console/console_template.js
@@ -31,7 +31,7 @@ function consoleHtml() {
         <!-- Static sidebar for desktop -->
         <div id="static-sidebar" class="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-48 lg:flex-col">
             <!-- Sidebar component, swap this element with another sidebar if you like -->
-            <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-black px-6 md:px-10 lg:px-6 pb-4 pt-20 md:pt-28 lg:pt-0">
+            <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-black px-6 md:px-10 lg:px-6 pb-4 pt-20 md:pt-32 lg:pt-0">
                 <div class="flex h-16 shrink-0 items-center cursor-pointer" onclick="navigationToHomePage()">
                 <img class="h-8 md:h-14 lg:h-8 w-auto" src="https://storage.googleapis.com/sketchmeai-public/branding/white_sketchmeai_logo.png" alt="Your Company">
                 </div>

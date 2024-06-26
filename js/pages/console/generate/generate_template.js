d26b6ef89ba25fb79dc0adb60e69a01d05e52a4d
Kevin Lopez <klopez@bu.edu>
2024-06-26 14:35:34 -0400

    adding guidance start and end fields to gen form, tweak10

--- js/pages/console/generate/generate_template.js
+++ js/pages/console/generate/generate_template.js
@@ -41,7 +41,7 @@ function newGridItemHTML(gen_id, gen_string="") {
                 <button class="absolute top-0 right-0 p-2 text-xs text-gray-400 hover:text-gray-500 hidden" id="cancel-button">Cancel</button>
             </div>
 
-            <div id="action-container" class="hidden rounded-lg bg-transparent group-hover:bg-gray-900 group-hover:bg-opacity-10 pointer-events-none group transition-bg-opacity duration-200">
+            <div id="action-container" class="hidden bg-transparent group-hover:bg-gray-900 group-hover:bg-opacity-10 pointer-events-none group transition-bg-opacity duration-200">
                 <button id="copy-button" class="absolute hidden lg:flex bottom-2 right-2 p-2 text-md text-white border-2 border-white opacity-100 lg:opacity-0 group-hover:opacity-100 hover:bg-gray-800 rounded-lg pointer-events-auto items-center justify-center transition-opacity duration-200">
                     <i class="fa fa-copy" aria-hidden="true"></i>
                 </button>
@@ -60,7 +60,7 @@ function newGridItemHTML(gen_id, gen_string="") {
 
             </div>
 
-            <div id="gen-menu-shield" class="bg-gray-900 bg-opacity-50 absolute top-0 left-0 w-full h-full hidden" onclick="tappedGenMenuShield(event)"></div>
+            <div id="gen-menu-shield" class="rounded-lg bg-gray-900 bg-opacity-50 absolute top-0 left-0 w-full h-full hidden" onclick="tappedGenMenuShield(event)"></div>
         
 			<div class="selection-overlay absolute inset-0 cursor-pointer pointer-events-none">
 				<div class="overlay-bg h-full w-full"></div>

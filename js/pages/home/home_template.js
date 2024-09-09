ecaa39e46ebdbd1521768ab0580ce44eb701b866
Kevin Lopez <klopez@bu.edu>
2024-09-09 18:33:25 -0400

    updating home page contents, tweak27

--- js/pages/home/home_template.js
+++ js/pages/home/home_template.js
@@ -119,8 +119,8 @@ function homePageHtml() {
 				<div class="lg:flex justify-between mx-auto max-w-7xl px-6 lg:px-8 items-center">
 					<div class="m-5 lg:m-10 mx-auto max-w-2xl text-center min-w-[40%]">
 						<p id="dev-log-str"></p>
-						<h1 class="text-4xl tracking-tight text-gray-900 sm:text-6xl">${headerTitle}</h1>
-						<p class="mt-6 text-lg leading-8 text-secondary text-justify">${headerSubtitle}</p>
+						<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-left">${headerTitle}</h1>
+						<p class="mt-6 text-lg leading-8 text-secondary text-left">${headerSubtitle}</p>
 						<div class="mt-10 flex flex-col items-start justify-left gap-x-6">
 							<a href="${headerButtonUrl}" class="rounded-md bg-black px-8 py-2.5 text-lg text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">${headerButtonTitle}</a>
 							<div class="mt-4">

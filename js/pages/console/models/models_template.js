6d4866b8ceb89f7eb90ee68d563c4c8a1f95e39a
klopez <klopez@bu.edu>
2025-12-06 15:34:01 -0500

    updating copy in new training model page, tweak4

--- js/pages/console/models/models_template.js
+++ js/pages/console/models/models_template.js
@@ -37,7 +37,7 @@ function dummyGridHTML() {
 
             <button id="reload-new-model-form" class="hidden" onclick="attemptToReloadSaveNewModelFormData()"></button>
             <h2 class="mb-1 text-gray-900 text-2xl">Train New Person Model</h2>
-            <p class="mb-5 text-gray-500 text-sm italic">using Flux[dev]</p>
+            <p class="mb-8 text-gray-500 text-sm">using Flux[dev]</p>
             <form id="new-form" autocomplete="off">
                 <div class="mt-0 grid grid-cols-1 gap-x-6 gap-y-2 sm:grid-cols-6">
                     <div class="col-span-2" id="name-field-container">
